5aa28dbed0da9fb17d5b45d14864bf1e63033846
ngocm <ngocm@192.168.1.10>
2020-04-13 08:15:08 +0700

    Example project made by Ming

---
+++ src/doan_vungocminh_qlns_test/DoAn_VuNgocMinh_QLNS_Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doan_vungocminh_qlns_test;
+
+/**
+ *
+ * @author ngocm
+ */
+public class DoAn_VuNgocMinh_QLNS_Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
